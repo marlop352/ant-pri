62c2900a8eb4769b4193ebbd7f007ac626b328fe
Marlus Lopes <reiescalibur@gmail.com>
2016-07-21 01:51:35 -0300

    signals code separated

--- wumpus3.pl
+++ wumpus3.pl
@@ -219,55 +219,6 @@ at_least_one_gold(E,AllSqrs,AllSqrs,GRestSqrs) :-
 %WNe
   addto_ww_init_state(gold(X,Y)).
 
-% stench(Stench): Stench = yes if wumpus (dead or alive) is in a square
-%   directly up, down, left, or right of the current agent location.
-
-stench(yes) :-
-  agent_location(X,Y),
-  X1 is X + 1,
-  X0 is X - 1,
-  Y1 is Y + 1,
-  Y0 is Y - 1,
-  ( wumpus_location(X1,Y) ;
-    wumpus_location(X0,Y) ;
-    wumpus_location(X,Y1) ;
-    wumpus_location(X,Y0) ;
-    wumpus_location(X,Y) ),
-  !.
-
-stench(no).
-
-
-% breeze(Breeze): Breeze = yes if a pit is in a square directly up, down,
-%   left, or right of the current agent location.
-
-breeze(yes) :-
-  agent_location(X,Y),
-  X1 is X + 1,
-  X0 is X - 1,
-  Y1 is Y + 1,
-  Y0 is Y - 1,
-  ( pit(X1,Y) ;
-    pit(X0,Y) ;
-    pit(X,Y1) ;
-    pit(X,Y0) ;
-    pit(X,Y)  ),
-  !.
-
-breeze(no).
-
-
-% glitter(Glitter): Glitter = yes if there is gold in the current agent
-%   location.
-
-glitter(yes) :-
-  agent_location(X,Y),
-  gold(X,Y),
-  !.
-
-glitter(no).
-
-
 % kill_wumpus: pretty obvious
 
 kill_wumpus :-
